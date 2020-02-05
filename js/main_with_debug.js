08459dbf7f168b6c682fad8a60cdc81fbca2c39a
reganmurray19 <56800621+reganmurray19@users.noreply.github.com>
2020-02-04 19:57:44 -0600

    Activity 3

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -24,7 +24,6 @@ function cities(){
 			population: 27244
 		}
 	];
-
 	//append the table element to the div
 	$("#mydiv").append("<table>");
 
@@ -42,47 +41,55 @@ function cities(){
         $("table").append(rowHtml);
     };
 
+    //call helper functions to fill web page
     addColumns(cityPop);
     addEvents();
 };
-
+//function to populate columns in the table
 function addColumns(cityPop){
 
+    //iterate through table rows
     $('tr').each(function(i){
 
+      //if the index is 0, append "City Size" to the header
     	if (i == 0){
 
-    		$(this).apend('<th>City Size</th>');
-    	} else {
+    		$(this).append('<th>City Size</th>');
 
+      //if it is not the header row, append row with
+      //descriptor of the city size
+    	} else {
     		var citySize;
 
+        //conditional statements assigning city size descriptors
     		if (cityPop[i-1].population < 100000){
     			citySize = 'Small';
 
     		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
+    			citySize = 'Medium';
 
     		} else {
     			citySize = 'Large';
     		};
-
-    		$this.append('<td' + citySize + '</td>');
+    		$(this).append('<td>' + citySize + '</td>');
     	};
     });
 };
 
+//function that updates webpage based on user interaction
 function addEvents(){
 
-	$('#table').mouseover(function(){
-
+  //function that causes changing colors when user
+  //hovers the mouse over the table
+	function mouseoverme(){
 		var color = "rgb(";
 
+    //loop that generates a random RGB color value
 		for (var i=0; i<3; i++){
 
 			var random = Math.round(Math.random() * 255);
 
-			color += "random";
+			color += random;
 
 			if (i<2){
 				color += ",";
@@ -91,14 +98,20 @@ function addEvents(){
 				color += ")";
 		};
 
+    //set the css color value to the random RGB color value
 		$(this).css('color', color);
-	});
+	 }
+  };
 
+ //attributes the mouseover function to the table
+  $('table').on('mouseover',mouseoverme);
+
+  //function that creates an alert when a click is applied
 	function clickme(){
-
 		alert('Hey, you clicked me!');
 	};
 
+  //attributes the click function to the table
 	$('table').on('click', clickme);
 };
 
